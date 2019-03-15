623477f91b4abfd168f711fba6d052bc86cbc857
salvoravida <salvoravida@gmail.com>
2019-03-15 12:45:49 +0100

    fix missing exports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Provider from './components/Provider'
 import connectAdvanced from './components/connectAdvanced'
+import { ReactReduxContext } from './components/Context'
 import connect from './connect/connect'
 
-export { Provider, connectAdvanced, connect }
+export { Provider, connectAdvanced, ReactReduxContext, connect }
